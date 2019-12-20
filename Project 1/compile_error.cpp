756f5024fa3c1f10770fb7e2a51d2ac29b49d0f4
Alex Chen <32936005+achen2289@users.noreply.github.com>
2019-12-19 17:56:20 -0800

    initial add

---
+++ Project 1/compile_error.cpp
@@ -0,0 +1,42 @@
+//
+//  compile_error.cpp
+//  1.3
+//
+//  Created by ALEX CHEN (: on 10/3/19.
+//  Copyright © 2019 cs. All rights reserved.
+//
+
+// Code for Project 1
+// Report poll results
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberSurveyed
+    int forImpeachment;
+    int antiImpeachment;
+
+    cout << "How many people were surveyed? ";
+    cin >> numberSurveyed;
+    cout << "How many of them support impeachment of the president? ";
+    cin >> forImpeachment;
+    cout << "How many of them oppose impeachment of the president? ";
+    cin >> antiImpeachment;
+
+    double pctFor = 100.0 * forImpeachment / numberSurveyed;
+    double pctAnti = 100.0 * antiImpeachment / numberSurveyed;
+
+    cout.setf(ios::fixed);
+    cout.precision(1);
+
+    cout << endl;
+    cout << pctFor << "% say they support impeachment." << endl;
+    cout << pctAnti << "% say they oppose impeachment." << endl;
+
+    if (forImpeachmen > antiImpeachment)
+        cout << "More people support impeachment than oppose it." << endl;
+    else
+        cout << "More people oppose impeachment than support it." << endl;
+}
